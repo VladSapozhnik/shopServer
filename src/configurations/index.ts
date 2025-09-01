b30465c8957349537fc31cc2c0ce4552869c4fc0
VladSapozhnik <vladbars2@gmaiil.com>
2025-09-01 20:55:39 +0300

    add cookie-parser and util is dev

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -9,5 +9,5 @@ export default () => ({
   jwt_access_token: process.env.JWT_ACCESS_TOKEN,
   jwt_refresh_token: process.env.JWT_REFRESH_TOKEN,
   cookie_domain: process.env.COOKIE_DOMAIN,
-  node_env: process.env.NODE_ENV,
+  is_dev: process.env.NODE_ENV,
 });
