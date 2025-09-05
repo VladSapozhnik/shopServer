f12741eeddd7486338a3a0f985d443543f5c0243
VladSapozhnik <vladbars2@gmaiil.com>
2025-09-05 18:06:02 +0300

    reform file config

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,10 +1,4 @@
 export default () => ({
-  port: process.env.PORT,
-  db_port: process.env.DB_PORT,
-  db_host: process.env.DB_HOST,
-  db_username: process.env.DB_USERNAME,
-  db_password: process.env.DB_PASSWORD,
-  db_name: process.env.DB_NAME,
   jwt_secret: process.env.JWT_SECRET,
   jwt_access_token: process.env.JWT_ACCESS_TOKEN,
   jwt_refresh_token: process.env.JWT_REFRESH_TOKEN,
